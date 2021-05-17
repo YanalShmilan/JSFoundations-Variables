40476fdc0f68f0e7aaca897e6f9b1274bbbb34ae
yanalshmilan <yshmilan@gmail.com>
2021-05-17 15:28:10 +0300

    done

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = 'yanal';
+let age = 23;
+let hobby = 'breathing';
+
+console.log(
+  `Mr./Mrs. ${name}, in ${age} years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
